d1de00ca553c0609540bc101e6392e8d7964829a
iamAbayomi <oladinifolawiyo@gmail.com>
2022-10-24 05:37:29 +0100

    refactor: move pages and styles folder directory  to src folder

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useRef } from "react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = useRef(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+          useErrorBoundary: false,
+          refetchOnWindowFocus: false
+        }
+      }
+    })
+  );
+
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient.current}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
