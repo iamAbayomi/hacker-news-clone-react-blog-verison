6dbafc79690fd54062034a676178f728ebffe568
iamAbayomi <oladinifolawiyo@gmail.com>
2022-10-26 16:59:24 +0100

    feat: implement default page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
+import { useGetCustomStories } from "../hooks";
 import AppLayout from "../layout";
 
 const Home = () => {
+  const { isLoading, customStories, fetchNextPage, isFetchingNextPage } =
+    useGetCustomStories("topstories", "news");
+
   return (
     <AppLayout
-      isLoading={false}
-      newsItems={[]}
-      isFetchingNextPage={false}
-      fetchNextPage={() => {}}
+      isLoading={isLoading}
+      newsItems={customStories}
+      isFetchingNextPage={isFetchingNextPage}
+      fetchNextPage={fetchNextPage}
     />
   );
 };
